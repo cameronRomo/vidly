9e6933117d53201b18c6feeeda8df8551f4a58e4
Cameron Romo <cromo1225@gmail.com>
2021-03-30 15:30:55 -0600

    Extract methods and components to clean up code

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -47,8 +47,7 @@ class Movies extends Component {
     this.setState({ sortColumn })
   }
 
-  render() { 
-    const { length: count } = this.state.movies;
+  getPageData = () => {
     const { 
       pageSize, 
       currentPage, 
@@ -57,9 +56,6 @@ class Movies extends Component {
       movies: allMovies 
     } = this.state;
 
-    if (count === 0) 
-      return <p>There are no movies in the database.</p>
-
     const filtered = selectedGenre && selectedGenre._id
       ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
       : allMovies;
@@ -67,6 +63,22 @@ class Movies extends Component {
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
 
     const movies = paginate(sorted, currentPage, pageSize);
+
+    return {totalCount: filtered.length, data: movies}
+  }
+
+  render() { 
+    const { length: count } = this.state.movies;
+    const { 
+      pageSize, 
+      currentPage, 
+      sortColumn, 
+    } = this.state;
+
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const {totalCount, data: movies} = this.getPageData();
+
 
     return ( 
       <div className="row">
@@ -78,7 +90,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing { filtered.length } movies in the database.</p>
+          <p>Showing { totalCount } movies in the database.</p>
           <MoviesTable 
             movies={movies} 
             onLike={this.handleLike} 
@@ -87,7 +99,7 @@ class Movies extends Component {
             sortColumn={sortColumn}
           />
           <Pagination 
-            itemsCount={filtered.length} 
+            itemsCount={totalCount} 
             pageSize={pageSize} 
             onPageChange={this.handlePageChange} 
             currentPage={currentPage}
