42df7dfc19263a6cf39d70ef51ca4625a403b468
Cameron Romo <cromo1225@gmail.com>
2021-04-06 16:06:17 -0600

    Add link for to connect the form based on specific id

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Like from './common/Like'
 import Table from './common/Table'
 
 class MoviesTable extends Component {
   columns = [
-    { path: 'title', label: 'Title' },
+    { path: 'title', label: 'Title', content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>},
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
@@ -13,7 +14,7 @@ class MoviesTable extends Component {
   ]
 
   render() { 
-      const { movies, onSort, sortColumn } = this.props
+    const { movies, onSort, sortColumn } = this.props
 
   return ( 
     <Table 
