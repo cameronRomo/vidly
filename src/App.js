5406c1f2b7623fc34c1428a314d88c93d896213f
Cameron Romo <cromo1225@gmail.com>
2021-04-06 14:06:28 -0600

    Add browser router, wrap with switch, and add exact path for home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Movies from './components/Movies';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
+import NotFound from './components/NotFound';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={ Movies }></Route>
+        <Route path="/customers" component={ Customers }></Route>
+        <Route path="/rentals" component={ Rentals }></Route>
+        <Route path="/not-found" component={ NotFound }></Route>
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
